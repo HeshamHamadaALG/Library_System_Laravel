44b80d25af8811af45e2227a96a6965fa7e0321c
Mohamed Shawkat <mohamedshawkat95@gmail.com>
2019-05-06 00:37:45 +0200

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', function () {
-        return redirect()->route('home');
+        return redirect()->route('books.index');
     });
     Route::get('/home', 'HomeController@index')->name('home');
 
