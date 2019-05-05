6c10cab448c0f1c2fc3cea30c8b723d86174f562
HeshamHamadaALG <heshamhamada222@gmail.com>
2019-05-05 15:02:57 +0200

    Hesham 17

--- config/database.php
+++ config/database.php
@@ -42,32 +42,13 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'libraryMS'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
         // 'mysql' => [
         //     'driver' => 'mysql',
-        //     'host' => env('DB_HOST', 'https://remotemysql.com'),
+        //     'host' => env('DB_HOST', '127.0.0.1'),
         //     'port' => env('DB_PORT', '3306'),
-        //     'database' => env('DB_DATABASE', 'VB4VZB0Yn8'),
-        //     'username' => env('DB_USERNAME', 'VB4VZB0Yn8'),
-        //     'password' => env('DB_PASSWORD', 'CCfVIKmTph'),
+        //     'database' => env('DB_DATABASE', 'libraryMS'),
+        //     'username' => env('DB_USERNAME', 'root'),
+        //     'password' => env('DB_PASSWORD', ''),
         //     'unix_socket' => env('DB_SOCKET', ''),
         //     'charset' => 'utf8mb4',
         //     'collation' => 'utf8mb4_unicode_ci',
@@ -79,6 +60,25 @@ return [
         //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
         //     ]) : [],
         // ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'https://remotemysql.com/phpmyadmin/index.php'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'VB4VZB0Yn8'),
+            'username' => env('DB_USERNAME', 'VB4VZB0Yn8'),
+            'password' => env('DB_PASSWORD', 'CCfVIKmTph'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
